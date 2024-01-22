b06231096f6ecfd367fb5350e757a29c202492c6
Said Gülmez <unseensenpai@hotmail.com>
2024-01-23 01:26:24 +0300

    Dummy data generator methods added. Intercepter added for update and create date (this one not necessary).

---
+++ src/Presentation/OnionSolution.API/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnionSolution.Application.Repositories.OrderRepository;
+using OnionSolution.Domain.Entities;
+
+namespace OnionSolution.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController(
+        IOrderReadRepository orderReadRepository,
+        IOrderWriteRepository orderWriteRepository) : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrderAsync(Order order)
+        {
+            var result = await orderWriteRepository.AddAsync(order);
+            await orderWriteRepository.SaveAsync();
+
+            return Ok(result);
+        }
+    }
+}
